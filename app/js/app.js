875310335cf5d4c018fa47c50996f14efbd96853
Peter Casinelli <pcas00@gmail.com>
2014-03-10 22:15:58 -0400

    Updated AngularJS
    Updated to latest AngularJS. First merge with previous movie
    recommender AngularJS.

--- app/js/app.js
+++ app/js/app.js
@@ -2,15 +2,14 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
+var recommenderApp = angular.module('recommenderApp', [
   'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  'recommenderControllers'
+]);
+
+
+recommenderApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'TopNMoviesCtrl'});
+  $routeProvider.when('/movies/:movieId', {templateUrl: 'partials/view-movie.html', controller: 'ViewMovieCtrl'});
+  $routeProvider.otherwise({redirectTo: '/home'});
 }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,123 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+var recommenderControllers = angular.module('recommenderControllers', []);
 
-  }])
-  .controller('MyCtrl2', [function() {
+recommenderControllers.controller('TopNMoviesCtrl', function($scope, $http) {
 
-  }]);
+  $scope.userProfile = undefined;
+  $scope.hasUserProfile = false;
+  $scope.isSignedIn = false;
+  $scope.immediateFailed = false;
+
+  $scope.signOut = function() {
+	  $scope.userProfile = undefined;
+	  $scope.hasUserProfile = false;
+	  $scope.isSignedIn = false;
+	  $scope.immediateFailed = false;
+  }	
+
+	  $scope.signIn = function(authResult) {
+	    $scope.$apply(function() {
+	      $scope.processAuth(authResult);
+	    });
+	  }
+	  
+	  $scope.processAuth = function(authResult) {
+	    $scope.immediateFailed = true;
+	    if ($scope.isSignedIn) {
+	      return 0;
+	    }
+	    if (authResult['access_token']) {
+	      $scope.immediateFailed = false;
+	      // Successfully authorized, create session
+	      console.log("Logged in with: " + angular.toJson(authResult));
+	      gapi.client.load('plus','v1', function(){
+			 var request = gapi.client.plus.people.get({
+			   'userId': 'me'
+			 });
+			 request.execute(function(resp) {
+			  	console.log("User id is: " + resp['id']);
+ 			  });
+		  });
+	      /*PhotoHuntApi.signIn(authResult).then(function(response) {
+	        $scope.signedIn(response.data);
+	      });*/
+	    } else if (authResult['error']) {
+	      if (authResult['error'] == 'immediate_failed') {
+	        $scope.immediateFailed = true;
+	      } else {
+	        console.log('Error:' + authResult['error']);
+	      }
+	    }
+	  }
+
+	$scope.renderSignIn = function() {
+		gapi.signin.render('myGsignin', {
+		      'callback': $scope.signIn,
+		      'clientid': '735820567734-melje1kfv51kthu56id1knllcekfn2kp.apps.googleusercontent.com',
+		      //'requestvisibleactions': Conf.requestvisibleactions,
+		      'scope': 'https://www.googleapis.com/auth/plus.login',
+		      'cookiepolicy': 'single_host_origin',
+		      // Remove the comment below if you have configured
+		      // appackagename in services.js
+		      //'apppackagename': Conf.apppackagename,
+		      'theme': 'dark',
+		    });
+	}
+
+	$scope.start = function() {
+    	$scope.renderSignIn();
+    }
+  
+
+	$http.get('http://localhost:8080/myapp/movies/db').success(function(data) {
+		//For each movie, append a movie image from IMDB image API
+		angular.forEach(data.movies, function(value, key) {
+			value.rating = Math.round(value.rating*10)/10;
+			var movieTitle = value.title.replace(/ /g, "+");
+			//Get first occurence of '('' to find year
+			var firstParenthesis = movieTitle.indexOf('(');
+			var year = -1;
+			if (firstParenthesis != -1) {
+				year = movieTitle.substring(firstParenthesis+1, firstParenthesis + 5);
+			}
+			/* Check to see if there is a comma
+			   E.g Shawshank Redemption, The (1994) => Shawshank Redemption
+			*/
+			var firstComma = movieTitle.indexOf(',');
+			movieTitle = (firstComma == -1) ? movieTitle : movieTitle.substring(0,firstComma);
+
+			var friendlyMovieTitle = (firstParenthesis == -1) ? movieTitle : movieTitle.substring(0, firstParenthesis-1);
+			var request = (year == -1) ? friendlyMovieTitle : friendlyMovieTitle + "&y=" + year;
+			//Use OMDBApi to request movie image using friendly title and year
+			$http.get('http://www.omdbapi.com/?t=' + request).success(function(movieData) {
+				value.image = movieData.Poster;
+			});
+
+		});
+
+		$scope.movies = data.movies;
+		console.log(data.movies);
+	});
+
+  	$scope.start();
+
+});
+
+/* View a movie by movie id */
+recommenderControllers.controller('ViewMovieCtrl', function($scope, $http, $routeParams) {
+	$scope.routeParams = $routeParams;
+	console.log("Find similar movies at: http://localhost:8080/myapp/movies/similar/" + $routeParams.movieId + "/3");
+	$http.get('http://localhost:8080/myapp/movies/similar/' + $routeParams.movieId + "/3").success(function(data) {
+		console.log(data);
+		//For each movie, get movie metadata from API
+		angular.forEach(data.movies, function(value, key) {
+			value.rating = Math.round(value.rating*10)/10;	
+		});
+
+		$scope.similarMovies = data.movies;
+	});
+
+
+});
